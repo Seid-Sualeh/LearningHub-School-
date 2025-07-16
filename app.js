415cf064a0435fb6ef8f3711a6074527e1df631b
Abu-muaz <plshireseid@gmail.com>
2025-07-16 22:44:16 +0300

    seid

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-
-const http = require('http'); 
-const server = http.createServer((req, res) => { 
-console.log(`Request URL: ${req.url}`); 
-console.log(`Request Method: ${req.method}`); 
-res.writeHead(200, { 'Content-Type': 'text/plain' }); 
-res.end('Request received'); 
-}); 
-server.listen(3000, () => { 
-console.log('Server is running on http://localhost:3000'); 
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Welcome to the Home Page</h1>");
+  } else if (req.url === "/about" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>About Us</h1>");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>404 - Page Not Found</h1>");
+  }
 });
- 
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
